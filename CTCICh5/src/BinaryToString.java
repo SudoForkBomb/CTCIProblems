d41882426a3bfa9fecd77914e84b9a49d7eab77d
Chris Taylor <chrisrtaylor123@gmail.com>
2017-01-21 23:27:30 -0500

    Believe I'm pretty much done with the solution for 5.2. Still need to check for any pattern and then exit from the whlie loop.

--- CTCICh5/src/BinaryToString.java
+++ CTCICh5/src/BinaryToString.java
@@ -6,14 +6,34 @@ public class BinaryToString {
     /* Instructions
     Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print the binary representation.
     If the number cannot be represented accurately in binary with at most 32 characters, print "ERROR:'
-
      */
 
     /* Questions
-
+        Since we are only doing numbers between 0 to 1, are we not doing a sign bit?
      */
 
     /* Solution
-
+        This helped. https://en.wikipedia.org/wiki/Binary_number#Decimal
      */
+
+    static public String binaryConvert(double realNum){
+        double result = realNum;
+        String binaryResult = "0.";
+        while(binaryResult.length() < 32){
+            result = result * 2;
+            System.out.println(result);
+
+            if(result > 1){
+
+                result = result - 1.0;
+                binaryResult = binaryResult + "1";
+            }
+            else{
+                binaryResult = binaryResult + "0";
+            }
+            System.out.println(binaryResult);
+        }
+
+        return binaryResult;
+    }
 }
---
+++ CTCICh5/src/main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by chris on 1/21/2017.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        double x = (0.1);
+        int y = 2;
+        System.out.println(x);
+
+        BinaryToString.binaryConvert(x);
+    }
+
+}
