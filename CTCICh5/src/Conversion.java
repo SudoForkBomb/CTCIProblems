8e817c7d2df327a419f24bb90f2c4dbd64560119
Chris Taylor <chrisrtaylor123@gmail.com>
2017-01-22 23:46:48 -0500

    Added additional information to 5.6

--- CTCICh5/src/Conversion.java
+++ CTCICh5/src/Conversion.java
@@ -6,6 +6,10 @@ public class Conversion {
     /* Instructions
     Write a function to determine the number of bits you would need to flip to convert integer A to integer B.
 
+    EXAMPLE
+    Input: 29 (or: 11101), 15 (or: 01111)
+    Output: 2
+
      */
 
     /* Questions
--- CTCICh5/src/NextNumber.java
+++ CTCICh5/src/NextNumber.java
@@ -22,5 +22,8 @@ public class NextNumber {
         String largeNext = "";
 
 
+
+
+
     }
 }
